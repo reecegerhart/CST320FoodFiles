2905d1740aacdffd20a4498f3c976c474e2f2a9d
Eschaffner <eschaffner05@gmail.com>
2025-12-02 23:17:30 -0700

    new

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
 
 public class SceneManagerScript : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(sceneName);
+        Debug.Log("Triggered by: " + other.name);
     }
 }
+
