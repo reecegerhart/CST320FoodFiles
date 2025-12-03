da6eda2982554fa388feb85159a9aadb20ab047b
Eschaffner <eschaffner05@gmail.com>
2025-12-03 00:26:21 -0700

    neww

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    public string sceneToLoad = "Scene2"; // Set this in Inspector
+    
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Triggered by: " + other.name);
+        // Check if the collider is the player
+        if (other.CompareTag("Player"))
+        {
+            // Load the next scene
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
 
